2316b212c0217e91e7fa45dd986fe91d3bcd2f24
Wes Lambert <wlambertts@gmail.com>
2019-06-07 19:42:07 +0000

    updates

--- misp2elastic/app/misp2elastic.py
+++ misp2elastic/app/misp2elastic.py
@@ -1,43 +1,41 @@
-import requests
 import time
 import yaml
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
-from pymemcache.client.base import Client
+import requests
+import requests
+from pymisp import PyMISP
 from config import parser
+from pymemcache.client.base import Client
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
-mem_host = parser.get('memcached', 'mem_host')
-mem_port = int(parser.get('memcached', 'mem_port'))
-client = Client((mem_host, mem_port))
-agetime = int(parser.get('memcached', 'agetime'))
-sleeptime = int(parser.get('memcached', 'sleeptime'))
-proto = parser.get('misp', 'misp_proto')
-misp_host = parser.get('misp', 'misp_host')
-apikey = parser.get('misp', 'misp_apikey')
+memcached_host = parser.get('memcached', 'host')
+memcached_port = int(parser.get('memcached', 'port'))
+memcached_agetime = int(parser.get('memcached', 'agetime'))
+memcached_sleeptime = int(parser.get('memcached', 'sleeptime'))
+memcached = Client((memcached_host, memcached_port))
+misp_url = parser.get('misp', 'url')
+misp_key = parser.get('misp', 'apikey')
+misp_verifycert = parser.getboolean('misp', 'verifycert')
 
 def getAttrs():
-
-    headers = {
-            'Authorization': apikey,
-            'Accept': 'application/json',
-            'Content-type': 'application/json',
-             }
-
+    def init(url,key):
+      return PyMISP(url, key, misp_verifycert, 'json')
+    misp = init(misp_url, misp_key)
+    call_path = 'attributes/restSearch'
     with open('misp2elastic.yaml', 'r') as f:
         mispyaml = yaml.safe_load(f)
         for i in mispyaml["iocs"]:
           for t in  mispyaml["iocs"][i]["tags"]:
-            data = '{"returnFormat":"text", "type":"' + i + '", "tags":"' + t + '"}'
-            response = requests.post(proto + '://' + misp_host + '/attributes/restSearch', headers=headers, data=data, verify=False)
-            items = (response.text).splitlines()
-            for item in items:
+            search = '{"returnFormat":"json", "type":"' + i + '", "tags":"' + t + '"}'
+            response = misp.direct_call(call_path, search)   
+            for event in response['response']['Attribute']:
+              item = event['value']
               if item != "":
-                memkey = i + '-' + item
+                memcached_key = i + '-' + item
                 try:
-                  client.set(memkey, t, agetime)
+                  memcached.set(memcached_key, t, memcached_agetime)
                 except: 
                   print("An error occurred!")
-    time.sleep(sleeptime)
-
+    time.sleep(memcached_sleeptime)
 while True:
     getAttrs()          
